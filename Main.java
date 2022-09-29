192747dcd5f4636c38b3826a71b09b3e3a5584b1
Zaid Jebril <107850242+ZaidJ2003@users.noreply.github.com>
2022-09-29 00:29:13 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         var obj = new BankingReport();
-        obj.printReport();
-        obj.getMenu();
+        obj.printReport(); obj.getMenu();
     } 
 }
